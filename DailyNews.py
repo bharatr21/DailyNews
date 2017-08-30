1199ba8168f58e17d868888bde0364d89edd7d0d
Bhk <bharatraghunthan9767@gmail.com>
2017-08-30 22:10:08 +0530

    Adds Image Fetcher along with Latest Headlines Fetcher

--- DailyNews.py
+++ DailyNews.py
@@ -4,16 +4,20 @@ import sys
 import re
 import bs4
 from apikeys import apikey
+import urllib
+import urllib.request
 #import time
 #t = time.process_time()
 cwd = os.getcwd()
 mypath = os.path.join(cwd,'DailyNews')
+imgpath = os.path.join(mypath,'Images')
 if not os.path.exists(mypath):
     os.makedirs(mypath)
-
+if not os.path.exists(imgpath):
+    os.makedirs(imgpath)
 KEY = apikey
 sites=['ars-technica','bbc-news','engadget','espn','espn-cric-info','google-news','ign','national-geographic','new-york-magazine','reddit-r-all','techcrunch','the-hindu','the-economist','the-new-york-times','the-times-of-india','the-wall-street-journal','the-washington-post','the-verge']
-times = ['top','latest']
+times = ['top','latest','popular']
 for i in range(len(sites)):
     for j in range(len(times)):
         payload={'source':sites[i],
@@ -25,8 +29,8 @@ for i in range(len(sites)):
             rso = r.json()
             if(rso['status']=='error'):
                 pass
-            #elif(KeyboardInterrupt):
-            #	sys.exit()
+            # elif(KeyboardInterrupt):
+            #     sys.exit()
             else:
                 rson = r.json()
                 f = open(os.path.join(mypath,str(rson['source']))+".txt","w+")
@@ -41,10 +45,19 @@ for i in range(len(sites)):
                     f.write('-----------------------------------------------------\n')
                     f.write('Author:-'+str(rson['articles'][k]['author'])+'\n')
                     f.write('-----------------------------------------------------\n')
+                    if rson['articles'][k]['urlToImage'] is not None:
+                        try:
+                        	os.chdir(imgpath)
+                        except (OSError,WindowsError):
+                        	os.chdir(mypath)
+                        try:
+                            urllib.request.urlretrieve(rson['articles'][k]['urlToImage'],'{}-{}'.format(rson['source'],k+1))
+                        except:
+	                        print('No images found for the {}th {} article.'.format(k+1,rson['source']))
                 f.close()
         except(req.exceptions.SSLError,req.packages.urllib3.exceptions.SSLError,req.exceptions.ConnectionError):
             print('Connection Failed.')
         except(UnicodeEncodeError):
-            print('Character Encoding Failed.')   
+            print('Character Encoding Failed.')
 #elapsed_time = time.process_time()-t
-#print("\nTime Taken: %ds\n"%(elapsed_time))            
+#print("\nTime Taken: %ds\n"%(elapsed_time))
